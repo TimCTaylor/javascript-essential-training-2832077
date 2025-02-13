a905e61b62a518bae3d02a97b819b8d17d9bed6e
Tim Taylor <editors@greyhartpress.com>
2025-02-13 18:06:56 +0000

    wip1

---
+++ Practice/03_12/Meal.js
@@ -0,0 +1,27 @@
+/**
+ * Creating classes:
+ *
+ * Class declaration: class Name {}
+ * Class expression:  const Name = class {}
+ */
+
+class Meal {
+    constructor(
+      // Defines parameters:
+      name,
+      calories,
+      cost
+    ) {
+      // Define properties:
+      this.name = name;
+      this.calories = calories;
+      this.cost  = cost;
+    }
+    // Add methods like normal functions:
+    setName(newName) {
+      this.name = newName;
+    }
+  }
+  
+  export default Meal;
+  
