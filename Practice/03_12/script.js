9fdc4f69c71d5ca7b19c3416dc15fc033aeb5dff
Tim Taylor <editors@greyhartpress.com>
2025-02-13 18:07:19 +0000

    wip2

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,25 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+import Meal from "./Meal";
+import Backpack from "./Backpack.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false
+);
+
+console.log("The everydayPack object:", everydayPack);
+console.log("The pocketNum value:", everydayPack.pocketNum);
+
+
+const myMeal = new Meal ("Pie & chips", 10, 5);
+console.log("myMeal = ", myMeal);
+myMeal.setName("New! Improved pie & chips");
+console.log("myMeal = ", myMeal);
